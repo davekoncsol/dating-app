ef00b93aec18d2c2e98ac3818b80ed40c219e009
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-10-04 13:22:20 -0700

    added routes

--- routes/api/scores.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scoresCtrl = require('../../controllers/scores');
-
-router.get('/', scoresCtrl.highScores);
-router.post('/', scoresCtrl.create);
-
-module.exports = router;
