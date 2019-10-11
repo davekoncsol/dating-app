af3c7c7251fb62599a9f0e6bae48992a9f00c95b
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-10-11 11:29:47 -0700

    trying to add socet IO messaging

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './pages/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
+
+
 
 ReactDOM.render(
 <Router><Route component={App}/></Router>, document.getElementById('root'));
