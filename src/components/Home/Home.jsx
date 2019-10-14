545aaf60bba20e78e391d971abc34eb15b0b0196
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-10-14 08:17:50 -0700

    updated homepage styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,14 +6,20 @@ function Body(props) {
     return (
         <div className='body'>
             <div className="home-top-image">
-                <h1>FIND THE RIGHT ONE FOR YOU</h1>
+                <h1>FIND SOMEONE FOR YOU</h1>
             </div>
-            <div className="home-content-1"></div>
+            <div className="home-content-1">
+                <h1>SOMEONE THAT GETS YOU FOR WHO YOU ARE</h1>
+            </div>
             <div className="home-middle-image"></div>
-            <div className="home-content-2"></div>
+            <div className="home-content-2">
+            <h1>SOMEONE THAT GETS YOU FOR WHO YOU ARE</h1>
+            </div>
             <div className="home-bottom-image"></div>
-            <div className="home-content-3"></div>
-            <h1>body</h1>
+            <div className="home-content-3">
+            <h1>SOMEONE THAT WILL LAST A LIFETIME</h1>
+            </div>
+            <h1>SIGN UP NOW</h1>
         </div>
     );
 }
