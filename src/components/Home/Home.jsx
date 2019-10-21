f04d08a1769c28054f350255d6de336ec002b6c8
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-10-21 15:09:15 -0400

    updated styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,10 +12,12 @@ function Body(props) {
             <div className="home-content-1">
                 <h1>SOMEONE THAT GETS YOU FOR WHO YOU ARE</h1>
             </div>
-            <div className="home-middle-image"></div>
-            <div className="home-content-2">
+            <div className="home-middle-image">
             <h1>SOMEONE THAT WILL LAST A LIFETIME</h1>
             </div>
+            {/* <div className="home-content-2">
+            
+            </div> */}
             <div className="home-bottom-image"></div>
             <div className="home-content-3">
             <Link to='/signup'>
